11673403850dd241229d98b64a229e50e1aa3d98
Кирилл <kirill.borisov@rt.ru>
2017-08-01 11:15:22 +0300

    findClientInfo

--- src/com/luxoft/bankapp/commands/FindClientCommand.java
+++ src/com/luxoft/bankapp/commands/FindClientCommand.java
@@ -1,5 +1,10 @@
 package com.luxoft.bankapp.commands;
 
+import com.luxoft.bankapp.exceptions.ClientExistsException;
+import com.luxoft.bankapp.model.Bank;
+import com.luxoft.bankapp.model.Client;
+
+import com.luxoft.bankapp.exceptions.BankException;
 /**
  * Created by Кирилл on 01.08.2017.
  */
@@ -7,12 +12,14 @@ public class FindClientCommand implements Command {
 
     private RequestCommand rCommand;
 
-    public printCommandInfo(RequestCommand rCommand){
+    public printCommandInfo(){
         this.rCommand = rCommand;
     }
 
-    public void execute() {
-        rCommand.buy();
+    public void execute(Client client,Bank bank ) throws ClientExistsException {
+
+        return bank.checkIfClientExists(client);
+
     }
 
 }
